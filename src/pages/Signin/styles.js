39a794e9713ad8629a991eff78e1d5a94b9c7b40
Pavlo Klymchuk <pavlo95klymchuk@gmail.com>
2021-11-17 17:56:57 +0200

    fixed singin and header adaptive

--- src/pages/Signin/styles.js
+++ src/pages/Signin/styles.js
@@ -1,16 +1,20 @@
 import { makeStyles } from '@material-ui/core';
 
 export const useStyles = makeStyles(() => ({
-  wrapper: {
-    maxWidth: '1000px',
-    margin: '0 auto',
-    padding: '1em calc(2vw + .5em) 2em',
-  },
-  title: {
-    margin: '0 0 .5em',
-    textAlign: 'center',
-  },
-  link: {
-    cursor: 'pointer',
-  },
+    wrapper: {
+        maxWidth: '1000px',
+        margin: '0 auto',
+        padding: '1em calc(2vw + .5em) 2em',
+    },
+    title: {
+        margin: '0 0 .5em',
+        textAlign: 'center',
+    },
+    link: {
+        cursor: 'pointer',
+    },
+    itemLogin: {
+        display: 'flex',
+        justifyContent: 'center'
+    }
 }));
